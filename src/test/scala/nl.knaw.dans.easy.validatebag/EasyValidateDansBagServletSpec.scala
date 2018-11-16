056e978974b7822c2e00f5cb2f9811d3546236f0
Stephan Hogenboom <stephan.hogenboom@dans.knaw.nl>
2018-11-16 15:38:17 +0100

    added body check for 200 not compliant test

--- src/test/scala/nl.knaw.dans.easy.validatebag/EasyValidateDansBagServletSpec.scala
+++ src/test/scala/nl.knaw.dans.easy.validatebag/EasyValidateDansBagServletSpec.scala
@@ -36,6 +36,8 @@ class EasyValidateDansBagServletSpec extends TestSupportFixture with ServletFixt
   it should "return a 200 and a response when presented an invalid bag" in {
     post(uri = s"/validate?infoPackageType=SIP&uri=file://${ bagsDir.path.toAbsolutePath }/metadata-correct", headers = Seq(("Accept", "Application/Json"))) {
       status shouldBe OK_200
+      body should include(" - [1.2.4(a)] bag-info.txt must contain exactly one 'Created' element; number found: 0")
+      body should include(" - [1.3.1(a)] Mandatory file 'manifest-sha1.txt' not found in bag.")
       body should include("Is compliant: false")
     }
   }
