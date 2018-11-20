ecd93819a61973cd2a45b1d9fb3f41155b251e8f
Stephan Hogenboom <stephan.hogenboom@dans.knaw.nl>
2018-11-20 14:27:29 +0100

    test response as json

--- src/test/scala/nl.knaw.dans.easy.validatebag/EasyValidateDansBagServletSpec.scala
+++ src/test/scala/nl.knaw.dans.easy.validatebag/EasyValidateDansBagServletSpec.scala
@@ -17,6 +17,7 @@ package nl.knaw.dans.easy.validatebag
 
 import java.net.URI
 
+import com.jayway.jsonpath.internal.JsonReader
 import org.apache.commons.configuration.PropertiesConfiguration
 import org.eclipse.jetty.http.HttpStatus.{ BAD_REQUEST_400, OK_200 }
 import org.scalatra.test.scalatest.ScalatraSuite
@@ -24,26 +25,34 @@ import org.scalatra.test.scalatest.ScalatraSuite
 class EasyValidateDansBagServletSpec extends TestSupportFixture with ServletFixture with ScalatraSuite  {
   private val app = new EasyValidateDansBagApp(Configuration("0", createProperties(), Seq(new URI("http://creativecommons.org/licenses/by-sa/4.0"))))
   private val validateBagServlet = new EasyValidateDansBagServlet(app)
+  private val jsonReader = new JsonReader()
   addServlet(validateBagServlet, "/*")
 
   "the validate handler" should "return a 200 and the response when presented a valid bag uri" in {
-    post(uri = s"/validate?infoPackageType=SIP&uri=file://${ bagsDir.path.toAbsolutePath }/valid-bag", headers = Seq(("Accept", "Application/Json"))) {
+    post(uri = s"/validate?infoPackageType=SIP&uri=file://${ bagsDir.path.toAbsolutePath }/valid-bag", headers = Seq(("Accept", "application/json"))) {
       status shouldBe OK_200
-      body should include("Is compliant: true")
+      info(body)
+      jsonReader.parse(body)
+      jsonReader.read("$.isCompliant").toString.toBoolean shouldBe true
+      jsonReader.read("$.infoPackageType").toString shouldBe "SIP"
     }
   }
 
   it should "return a 200 and a response including 'compliant: false' and reasons when presented an invalid bag" in {
-    post(uri = s"/validate?infoPackageType=SIP&uri=file://${ bagsDir.path.toAbsolutePath }/metadata-correct", headers = Seq(("Accept", "Application/Json"))) {
+    post(uri = s"/validate?infoPackageType=SIP&uri=file://${ bagsDir.path.toAbsolutePath }/metadata-correct", headers = Seq(("Accept", "application/json"))) {
       status shouldBe OK_200
-      body should include(" - [1.2.4(a)] bag-info.txt must contain exactly one 'Created' element; number found: 0")
-      body should include(" - [1.3.1(a)] Mandatory file 'manifest-sha1.txt' not found in bag.")
-      body should include("Is compliant: false")
+      jsonReader.parse(body)
+      jsonReader.read("$.bagUri").toString shouldBe s"file://${ bagsDir.path.toAbsolutePath }/metadata-correct"
+      jsonReader.read("$.bag").toString shouldBe "metadata-correct"
+      jsonReader.read("$.profileVersion").toString shouldBe "0"
+      jsonReader.read("$.infoPackageType").toString shouldBe "SIP"
+      jsonReader.read("$.ruleViolations.[0]").toString shouldBe "{1.2.4(a)=bag-info.txt must contain exactly one 'Created' element; number found: 0}"
+      jsonReader.read("$.ruleViolations.[1]").toString shouldBe "{1.3.1(a)=Mandatory file 'manifest-sha1.txt' not found in bag.}"
     }
   }
 
   it should "return a 400 if presented a non existing bag uri" in {     //TODO shouldn't this return a 404?
-    post(uri = s"/validate?infoPackageType=SIP&uri=file://${ bagsDir.path.toAbsolutePath }/_._metadata-correct", headers = Seq(("Accept", "Application/Json"))) {
+    post(uri = s"/validate?infoPackageType=SIP&uri=file://${ bagsDir.path.toAbsolutePath }/_._metadata-correct", headers = Seq(("Accept", "application/json"))) {
       status shouldBe BAD_REQUEST_400
       body should include("Bag does not exist")
     }
