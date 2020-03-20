e956b24b42b7ec74e70f9b36c85f8ec30d27244e
Jo Pol <jo-pol@users.noreply.github.com>
2020-03-20 07:08:16 +0100

    Update src/test/scala/nl.knaw.dans.easy.validatebag/rules/metadata/MetadataRulesSpec.scala
    Co-Authored-By: Richard van Heest <rvanheest@users.noreply.github.com>

--- src/test/scala/nl.knaw.dans.easy.validatebag/rules/metadata/MetadataRulesSpec.scala
+++ src/test/scala/nl.knaw.dans.easy.validatebag/rules/metadata/MetadataRulesSpec.scala
@@ -148,7 +148,7 @@ class MetadataRulesSpec extends TestSupportFixture with CanConnectFixture {
 
   private val allRules: Seq[NumberedRule] = {
     val xmlValidator = new XmlValidator(null) {override def validate(is: InputStream): Try[Unit] = Success(()) }
-    val validatorMap = { Map("dataset.xml" -> xmlValidator, "files.xml" -> xmlValidator, "agreements.xml" -> xmlValidator) }
+    val validatorMap = Map("dataset.xml" -> xmlValidator, "files.xml" -> xmlValidator, "agreements.xml" -> xmlValidator)
     ProfileVersion0.apply(validatorMap, allowedLicences = Seq.empty, BagStore(new URI(""), 1000, 1000))
   }
 
